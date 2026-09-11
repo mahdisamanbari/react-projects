f4c4e1b1af5208975b4a835607cc152511dcd647
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2026-09-11 14:41:09 +0330

    feat: implement scroll indicator logic

--- scroll-indicator/src/ScrollIndicator.jsx
+++ scroll-indicator/src/ScrollIndicator.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function ScrollIndicator() {
+    const[scrollTop , setScrollTop]=useState(0)
+    const onScroll = ()=>{
+        const winScroll= document.documentElement.scrollTop
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        const scrolled = (winScroll / height) * 100
+        setScrollTop(scrolled)
+
+    }
+    useEffect(()=>{
+        window.addEventListener('scroll',onScroll)
+    },[])
   return (
     <div className='wrapper'>
         <div className="progressbar">
-            <div className="progressbarmain">
+            <div className="progressbarmain" style={{width : `${scrollTop}%`}}>
                 
             </div>
         </div>
